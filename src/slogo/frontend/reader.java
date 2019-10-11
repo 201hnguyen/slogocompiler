3bca264d0b69eafb46805dd90d50f9d860a6e8fe
Amber Johnson <ajj18@duke.edu>
2019-10-11 19:29:11 -0400

    front-end package added. Some front-end classes and displayAnimation interfaces added

---
+++ src/slogo/frontend/reader.java
@@ -0,0 +1,32 @@
+package slogo.frontend;
+
+/**
+ * Simple parser based on regular expressions that matches program strings to
+ * kinds of language features.
+ *
+ * @author Robert Duvall
+ */
+
+public class reader {
+
+    /**
+     * Create an empty parser
+     */
+    public reader () {}
+
+    /**
+     * Adds the given resource file to this language's recognized types
+     */
+    public void addPatterns (String syntax) {}
+
+    /**
+     * Returns language's type associated with the given text if one exists
+     */
+    public String getSymbol (String text) {
+        return null;
+    }
+
+
+
+
+}
