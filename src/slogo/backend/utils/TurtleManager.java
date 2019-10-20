bc1702174b959800aa990a53e0c3476c878e780a
Erie Seong Ho Han <eh174@duke.edu>
2019-10-20 02:35:28 -0400

    fixing TurtleManger

--- src/slogo/backend/utils/TurtleManager.java
+++ src/slogo/backend/utils/TurtleManager.java
@@ -1,11 +1,15 @@
 package slogo.backend.utils;
 
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TurtleManager {
+
+    private static final String TURTLE_IMAGE_PATH = "../../../resources/image/turtle.jpg";
+
     private List<Turtle> myTurtles = new ArrayList<>();
     private Pane myTurtlePane;
 
@@ -14,13 +18,16 @@ public class TurtleManager {
         myTurtlePane = myPane;
     }
 
-    public Turtle getTurtle(String turtleID) throws NullPointerException{
+    public Turtle getTurtle(String turtleID){
         for(Turtle turtle : myTurtles) {
             if(turtle.getMyID().equals(turtleID)) {
                 return turtle;
             }
         }
-        myTurtles.add(new Turtle())
+        Turtle newTurtle = new Turtle(new Image(TURTLE_IMAGE_PATH, 10, 10, true, true), turtleID);
+        myTurtles.add(newTurtle);
+        myTurtlePane.getChildren().add(newTurtle);
+        return newTurtle;
         //return null; there will be an exception later.
         //return myTurtles.get(0); // for testing.
     }
@@ -41,7 +48,6 @@ public class TurtleManager {
         Turtle removeTurtle = getTurtle(turtleID);
         myTurtles.remove(removeTurtle);
         myTurtlePane.getChildren().remove(removeTurtle);
-
         /**
          * TODO: add new turtle to myTurtlePane and myTurtles
          */
@@ -50,5 +56,4 @@ public class TurtleManager {
     public void initialize() {
 
     }
-
 }
