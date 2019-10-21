b12dce1502351bb5cfcff65e700ff60bbf483e4b
Erie Seong Ho Han <eh174@duke.edu>
2019-10-20 21:53:42 -0400

    Main class edited to make program work

--- src/slogo/Main.java
+++ src/slogo/Main.java
@@ -11,15 +11,32 @@ import slogo.frontend.Visualization;
 import java.util.ArrayList;
 import java.util.List;
 
+
+public class Main extends Application{
+
+    /**
+     * Start of the program.
+     */
+    public static void main (String[] args) {
+        launch(args);
+//        Repeat repeat = new Repeat();
+//        List<Object> parameters = new ArrayList<>();
+//        parameters.add(3);
+//        parameters.add("fd 50 fd 10"); // FIXME: currently split by comma just for sake of demo
+//        repeat.execute(parameters);
+//        CommandBlockManager commandBlockManager = new CommandBlockManager("Forward 50 IfElse LessThan 5 50 [ Forward 10 ] [ Forward 20 ] Forward 30", turtleManager);
+//        CommandBlockManager commandBlockManager = new CommandBlockManager("Forward 50 Repeat 3 [ Forward 10 ] Forward 30", turtleManager);
+        //CommandBlockManager commandBlockManager = new CommandBlockManager("Sum 10 50 If LessThan 5 50 [ Sum 10 10 ] ", turtleManager);
+//        CommandBlockManager commandBlockManager = new CommandBlockManager("MakeVariable :a Sum 10 10 ", turtleManager);
+    }
+
+
     @Override
     public void start(Stage stage) throws Exception {
         Visualization visualization = new Visualization(stage);
         visualization.helpHost = getHostServices();
         TurtleManager turtleManager = visualization.getTurtle();
         CommandBlockManager commandBlockManager = new CommandBlockManager("Forward 50", turtleManager);
-        commandBlockManager.executeInstructionBlock();
-    }
-
         commandBlockManager.executeInstructionBlock();
     }
 }
