94612c67039e1da21b863f09330ed96f539af98e
Amber Johnson <ajj18@duke.edu>
2019-10-24 16:41:45 -0400

    exception class renamed

--- src/slogo/backend/ParserException.java
+++ src/slogo/backend/ParserException.java
@@ -1,16 +1,16 @@
 package slogo.backend;
 
-public class cmdException {
+public class ParserException {
 
     //constructor 1
     //Thrown when user-input language does not match language pattern
-    public cmdException(String language) {
+    public ParserException(String language) {
 
     }
 
     //constructor 2
     //Thrown when user-input command does not match a command in the given language
-    public cmdException(String language, String cmd) {
+    public ParserException(String language, String cmd) {
 
     }
 
