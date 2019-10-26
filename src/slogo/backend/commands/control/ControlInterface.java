41d4dfcca96eb782fc02861a1d6f1a54ca96e508
Ha Nguyen <ha.nguyen@duke.edu>
2019-10-26 16:17:22 -0400

    Fixed small bug in adding variables

--- src/slogo/backend/commands/control/ControlInterface.java
+++ src/slogo/backend/commands/control/ControlInterface.java
@@ -6,5 +6,5 @@ import java.util.List;
 import java.util.Map;
 
 public interface ControlInterface {
-    double execute(TurtleHistory turtleHistory, List<Object> parameters, Map<String, Double> variables);
+    double execute(TurtleHistory turtleHistory, List<Object> parameters, List<Map<String, Double>> accessibleVariables);
 }
