3ec5b082bf08fa7a59f2b756d10e0f6303ea8130
Ha Nguyen <ha.nguyen@duke.edu>
2019-10-29 18:46:37 -0400

    Fixed bug in Ask and and Tell to execute operations for turtle ids

--- src/slogo/backend/commands/control/controlcommands/Tell.java
+++ src/slogo/backend/commands/control/controlcommands/Tell.java
@@ -1,10 +1,12 @@
 package slogo.backend.commands.control.controlcommands;
 
+import slogo.backend.commands.CommandBlockManager;
+import slogo.backend.commands.PeekableScanner;
 import slogo.backend.commands.control.ControlInterface;
+import slogo.backend.utils.CommandTree;
 import slogo.backend.utils.TurtleHistory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -13,11 +15,22 @@ public class Tell implements ControlInterface {
     public double execute(TurtleHistory turtleHistory, List<Object> parameters, List<Map<String, Double>> accessibleVariables, Map<String, List<Object>> definedFunctions) {
         String turtlesToActivateArgument = parameters.get(0).toString();
 
-        List<String> turtlesToActivateStringList = Arrays.asList(turtlesToActivateArgument.split(" "));
         List<Integer> turtlesToActivate = new ArrayList<>();
-        for (String turtleID : turtlesToActivateStringList) {
-            turtlesToActivate.add(Integer.parseInt(turtleID));
+        CommandTree commandTree = new CommandTree(turtleHistory);
+        PeekableScanner turtlesScanner = new PeekableScanner(turtlesToActivateArgument);
+        while (turtlesScanner.hasNext()) {
+            while (!commandTree.onlyNumberLeft()) {
+                String turtleArgument = turtlesScanner.next();
+                turtleArgument = CommandBlockManager.checkAndInputUserVariable(turtleArgument, accessibleVariables);
+                try {
+                    commandTree.addToCommandTree(turtleArgument);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace(); //FIXME
+                }
+            }
+            turtlesToActivate.add((int) commandTree.getLastDouble());
         }
+
         turtleHistory.setActiveTurtles(turtlesToActivate);
         for (Integer id : turtleHistory.getActiveTurtles()) {
             System.out.println("TELL ACTIVATED TURTLE" + id);
